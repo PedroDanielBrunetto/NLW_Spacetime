20e7df0604ef309a3ea4043f6820fc694fcf13fb
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-07-05 16:22:41 -0300

    pt2-vd3-commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,17 @@
 /* eslint-disable prettier/prettier */
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // todas URLs de front-end poderao acessar nosso back-end
 })
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app.listen({
